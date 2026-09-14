8c511e9e98a6d6bce4ccb096946e7c4185d0d075
aryanbrite <aryanbrite@gmail.com>
2026-09-14 12:55:55 +0530

    added login

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "this is home page"
+
+
+@app.route("/login")
+def login():
+    
